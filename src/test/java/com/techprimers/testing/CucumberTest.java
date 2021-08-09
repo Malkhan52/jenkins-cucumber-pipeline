ed3a6defdcfdd9db73a8da29a8019dfa500e3989
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-08-09 19:15:57 +0530

    latest11 commit

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -6,10 +6,21 @@ import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @Cucumber
+//@CucumberOptions(
+//		features = "src/test/resources/features/bagbasics",
+//		glue= {"io.tpd.springbootcucumber.bagbasics"},		
+//		plugin = { "pretty", "json:target/cucumber-reports/cucumber.json",
+//						"junit:target/cucumber-reports/cucumber.xml",
+//						"html:target/cucumber-reports"},
+//		monochrome = true
+//	)
 @CucumberOptions(
         features = {"src/test/resources"},
         glue = {"com.techprimers.testing"},
-        plugin = {"pretty", "html:target/cucumber"}
+        plugin = { "pretty", "json:target/cucumber-reports/cucumber.json",
+				"junit:target/cucumber-reports/cucumber.xml",
+				"html:target/cucumber-reports"},
+        monochrome = true
 
 		)
 public class CucumberTest {

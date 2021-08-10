408cacadea7a0d700217f6c8b38e85735813db96
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-08-10 11:06:24 +0530

    latest15 commit

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -1,27 +1,16 @@
 package com.techprimers.testing;
 
-//plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
-
-import io.cucumber.junit.platform.engine.Cucumber;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 
-@Cucumber
-//@CucumberOptions(
-//		features = "src/test/resources/features/bagbasics",
-//		glue= {"io.tpd.springbootcucumber.bagbasics"},		
-//		plugin = { "pretty", "json:target/cucumber-reports/cucumber.json",
-//						"junit:target/cucumber-reports/cucumber.xml",
-//						"html:target/cucumber-reports"},
-//		monochrome = true
-//	)
-@CucumberOptions(
-        features = {"src/test/resources"},
-        glue = {"com.techprimers.testing"},
-        plugin = { "pretty", "json:target/cucumber-reports/cucumber.json",
-				"junit:target/cucumber-reports/cucumber.xml",
-				"html:target/cucumber-reports"},
-        monochrome = true
+//plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
 
-		)
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"json:target/cucumber.json"})
 public class CucumberTest {
+
 }
+
